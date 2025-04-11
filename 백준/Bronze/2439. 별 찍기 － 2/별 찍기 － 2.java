8f47aed1d56d6ed6475ef5a314e7fc1551dde29e
barrackkk <kgmlwns904@gmail.com>
2025-04-11 16:12:41 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 232 ms, Memory: 19904 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+
+        for(int i = 0 ; i <  a; i++){
+            for(int j = 0 ;  j < a - i - 1 ; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0 ; k <= i ; k++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
