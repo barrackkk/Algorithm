ae3af4ad12686c398c12c18639e08e24a7ce479c
barrackkk <kgmlwns904@gmail.com>
2025-04-21 14:17:18 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 708 ms, Memory: 51408 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int arr[][] = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[i][0] = x;
+            arr[i][1] = y;
+        }
+        br.close();
+
+        Arrays.sort(arr, (x, y) -> {
+            if (x[1] == y[1]) {
+                return x[0] - y[0];
+            } else {
+                return x[1] - y[1];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+
+        }
+        bw.flush();
+        bw.close();
+
+    }
+}
