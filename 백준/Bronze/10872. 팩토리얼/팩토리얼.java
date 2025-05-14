bd0fe1511c641ef19e71dbc787190b6ff95b2461
barrackkk <kgmlwns904@gmail.com>
2025-05-14 20:04:25 +0900

    [Bronze III] Title: 팩토리얼, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -4,20 +4,19 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-
-    public static int recursive(int a) {
+    public static int rec(int a) {
         if (a <= 1) {
             return 1;
-        } else {
-            return a * recursive(a - 1);
         }
+
+        return a * rec(a - 1);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
 
-        System.out.println(recursive(a));
+        int a = Integer.parseInt(br.readLine());
+        System.out.println(rec(a));
 
     }
 }
