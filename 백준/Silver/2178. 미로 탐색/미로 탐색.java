0ace1f23b59b10b57eaf07ed26f3d5bae5ab7fef
barrackkk <kgmlwns904@gmail.com>
2025-05-22 21:17:23 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14636 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0}; // 상하좌우
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int cx = now[0];
+            int cy = now[1];
+
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = cx + dx[dir];
+                int ny = cy + dy[dir];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (!visited[nx][ny] && map[nx][ny] == 1) {
+                        visited[nx][ny] = true;
+                        map[nx][ny] = map[cx][cy] + 1;
+                        q.offer(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String row = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = row.charAt(j) - '0';
+            }
+        }
+
+        bfs(0, 0);
+        System.out.println(map[n - 1][m - 1]);
+        
+    }
+}
