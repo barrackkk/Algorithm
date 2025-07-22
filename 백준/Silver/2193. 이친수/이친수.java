4746ffc17405f8a56381f674431a07ec94a59d68
barrackkk <kgmlwns904@gmail.com>
2025-07-22 13:31:05 +0900

    [Silver III] Title: 이친수, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp0 = new long[N + 1];
+        long[] dp1 = new long[N + 1];
+
+        dp0[1] = 0;
+        dp1[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp0[i] = dp0[i - 1] + dp1[i - 1];
+            dp1[i] = dp0[i - 1];
+        }
+        System.out.println(dp0[N] + dp1[N]);
+    }
+}
