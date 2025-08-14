fe47d03ed32998bc11c81696fad86cca74633173
barrackkk <kgmlwns904@gmail.com>
2025-08-14 13:47:24 +0900

    [Silver IV] Title: 주몽, Time: 380 ms, Memory: 17872 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        boolean[] visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            visited[i] = false;
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] + arr[j] == M && !visited[i] && !visited[j]) {
+                    cnt++;
+                    visited[i] = true;
+                    visited[j] = true;
+                }
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
