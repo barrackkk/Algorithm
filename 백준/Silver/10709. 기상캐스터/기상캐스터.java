cb292ae403e8183628a1774be67c744755065378
barrackkk <kgmlwns904@gmail.com>
2025-09-04 00:35:20 +0900

    [Silver V] Title: 기상캐스터, Time: 116 ms, Memory: 18472 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.java
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -1,48 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         int H = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
 
+        char[][] arr = new char[H][W];
         for (int i = 0; i < H; i++) {
             String line = br.readLine();
-            List<Character> list = new ArrayList<>();
             for (int j = 0; j < W; j++) {
-                list.add(line.charAt(j));
+                arr[i][j] = line.charAt(j);
             }
+
             int cnt = 0;
             boolean hasC = false;
-            List<Integer> result = new ArrayList<>();
+            int[][] result = new int[H][W];
 
             for (int j = 0; j < W; j++) {
-                char c = list.get(j);
+                char c = arr[i][j];
                 if (c == 'c') {
-                    result.add(0);
+                    result[i][j] = 0;
                     hasC = true;
                     cnt = 0;
                 } else if (hasC && c != 'c') {
                     cnt++;
-                    result.add(cnt);
+                    result[i][j] = cnt;
                 } else {
-                    result.add(-1);
+                    result[i][j] = -1;
                 }
+                sb.append(result[i][j]).append(" ");
             }
-
-            for (Integer integer : result) {
-                System.out.print(integer + " ");
-            }
-            System.out.println();
-
+            sb.append("\n");
         }
 
+        System.out.println(sb);
     }
 }
