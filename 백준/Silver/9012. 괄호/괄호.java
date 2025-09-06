fcedc002f4ffa076e80d527e3c11ffd21c3560ae
barrackkk <kgmlwns904@gmail.com>
2025-09-06 13:28:32 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,46 +1,43 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-        int n = Integer.parseInt(br.readLine());
-        int sum = 0;
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < n; i++) {
-            String input = br.readLine();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < N; t++) {
             Stack<Character> stk = new Stack<>();
-            for (int j = 0; j < input.length(); j++) {
-                char c = input.charAt(j);
+            String line = br.readLine();
+            boolean tf = true;
+            for (int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
 
                 if (c == '(') {
                     stk.push(c);
-                } else if (c == ')') {
-
-                    if (stk.empty()) {
-                        stk.push(c);
-                    } else if (stk.peek().equals('(')) {
+                }
+                if (c == ')') {
+                    if (!stk.isEmpty() && stk.peek() == '(') {
                         stk.pop();
-                    } else if (stk.peek().equals(')')) {
-                        stk.push(c);
+                    } else if (stk.isEmpty()) {
+                        tf = false;
+                        break;
                     }
                 }
             }
-            if (stk.empty()) {
+
+            if (stk.isEmpty() && tf) {
                 sb.append("YES\n");
             } else {
                 sb.append("NO\n");
             }
-            stk.clear();
-
         }
-        System.out.println(sb);
 
+        System.out.println(sb);
     }
 }
