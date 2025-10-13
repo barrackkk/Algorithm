1697fa468c228db7e817c3b571b4f589dab1fa1d
barrackkk <kgmlwns904@gmail.com>
2025-10-14 00:32:03 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16448 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,21 +13,27 @@ public class Main {
         int M = Integer.parseInt(br.readLine());
 
         int[] arr = new int[N];
-        boolean[] visited = new boolean[N];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            visited[i] = false;
+
         }
 
         int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (arr[i] + arr[j] == M && !visited[i] && !visited[j]) {
-                    cnt++;
-                    visited[i] = true;
-                    visited[j] = true;
-                }
+        int left = 0;
+        int right = N - 1;
+        Arrays.sort(arr);
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == M) {
+                cnt++;
+                left++;
+                right--;
+            } else if (sum < M) {
+                left++;
+            } else {
+                right--;
             }
         }
         System.out.println(cnt);
