f48e4375f28ccd8b026682ea35126df480953970
barrackkk <kgmlwns904@gmail.com>
2025-12-19 08:51:16 +0900

    [Silver V] Title: 사과 담기 게임, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
@@ -3,40 +3,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N, M, J;
-
+    static int N, cnt, M, K, areaCnt;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static int[][] map;
+    static boolean[][] visited;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int J = Integer.parseInt(br.readLine());
 
-        J = Integer.parseInt(br.readLine());
-
+        int cnt = 0;
         int left = 1;
         int right = M;
-        int cnt = 0;
-        for (int i = 0; i < J; i++) {
-            int num = Integer.parseInt(br.readLine());
 
-            if (num < left) {
-                int d = left - num;
+        for (int i = 0; i < J; i++) {
+            int idx = Integer.parseInt(br.readLine());
+
+            if (idx < left) {
+                int d = left - idx;
                 cnt += d;
                 left -= d;
                 right -= d;
-            } else if (num > right) {
-                int d = num - right;
+            }
+            if (idx > right) {
+                int d = idx - right;
                 cnt += d;
                 left += d;
                 right += d;
             }
         }
-        System.out.println(cnt);
+        sb.append(cnt);
+        System.out.println(sb);
 
     }
-
-
 }
