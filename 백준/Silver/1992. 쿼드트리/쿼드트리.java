f72f6969748663737e4b417bb4b5760a9a4df928
barrackkk <kgmlwns904@gmail.com>
2025-12-19 08:34:47 +0900

    [Silver I] Title: 쿼드트리, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -2,57 +2,60 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    static int N;
+    static int N, cnt, M, K, areaCnt;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
     static int[][] map;
+    static boolean[][] visited;
     static StringBuilder sb = new StringBuilder();
 
-    static void rec(int x1, int y1, int x2, int y2) {
+    public static void go(int x1, int y1, int x2, int y2) {
         int first = map[x1][y1];
-        boolean uniform = true;
-        for (int i = x1; i < x2; i++) {
-            for (int j = y1; j < y2; j++) {
-                if (map[i][j] != first) {
-                    uniform = false;
+        boolean unique = true;
+
+        for (int x = x1; x < x2; x++) {
+            for (int y = y1; y < y2; y++) {
+                if (first != map[x][y]) {
+                    unique = false;
                     break;
                 }
             }
-            if (!uniform) {
+            if (!unique) {
                 break;
             }
         }
 
-        if (uniform) {
+        if (unique) {
             sb.append(first);
             return;
         }
-
-        sb.append('(');
+        sb.append("(");
 
         int mx = (x1 + x2) / 2;
         int my = (y1 + y2) / 2;
-        rec(x1, y1, mx, my);
-        rec(x1, my, mx, y2);
-        rec(mx, y1, x2, my);
-        rec(mx, my, x2, y2);
+        go(x1, y1, mx, my);
+        go(x1, my, mx, y2);
+        go(mx, y1, x2, my);
+        go(mx, my, x2, y2);
 
-        sb.append(')');
+        sb.append(")");
     }
 
     public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         map = new int[N][N];
-
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < N; j++) {
-                map[i][j] = s.charAt(j) - '0';
+                char c = line.charAt(j);
+                map[i][j] = c - '0';
             }
         }
+        go(0, 0, N, N);
 
-        rec(0, 0, N, N);
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
