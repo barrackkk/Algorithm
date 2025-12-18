0c24b0f79e2400b79145e689fe23b2f85109defc
barrackkk <kgmlwns904@gmail.com>
2025-12-18 15:48:12 +0900

    [Silver I] Title: 영역 구하기, Time: 112 ms, Memory: 15120 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,80 +1,80 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N, M, K;
+    static int N, cnt, M, K, areaCnt;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
     static int[][] map;
     static boolean[][] visited;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
+    static List<Integer> list = new ArrayList<>();
 
-    static int dfs(int x, int y) {
+
+    private static int dfs(int x, int y) {
         visited[x][y] = true;
-
-        int area = 1;
+        areaCnt = 1;
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
 
-            if (nx >= 0 && ny >= 0 && nx < M && ny < N) {
-                if (map[nx][ny] == 0 && !visited[nx][ny]) {
+            if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                if (!visited[nx][ny] && map[nx][ny] == 0) {
                     visited[nx][ny] = true;
-                    area += dfs(nx, ny);
+                    areaCnt += dfs(nx, ny);
+
                 }
             }
         }
-        return area;
+        return areaCnt;
+
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        map = new int[M][N];
-        visited = new boolean[M][N];
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
+
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
-
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            for (int j = x1; j < x2; j++) {
-                for (int k = y1; k < y2; k++) {
-                    map[k][j] += 1;
+            for (int x = x1; x < x2; x++) {
+                for (int y = y1; y < y2; y++) {
+                    map[x][y] = 1;
                 }
             }
         }
 
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visited[i][j] && map[i][j] == 0) {
-                    result.add(dfs(i, j));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0 && !visited[i][j]) {
+                    list.add(dfs(i, j));
+                    cnt++;
                 }
             }
         }
-        bw.write(String.valueOf(result.size()) + '\n');
-        result.sort(Comparator.naturalOrder());
-        for (Integer i : result) {
-            bw.write(i + " ");
-        }
-        bw.flush();
-        bw.close();
+        sb.append(cnt).append("\n");
 
+        list.sort(Comparator.naturalOrder());
+        for (Integer i : list) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
     }
 }
