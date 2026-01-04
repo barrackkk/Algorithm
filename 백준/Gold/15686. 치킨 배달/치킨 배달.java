cf398f204011225bc8f664906a758530a450e3b6
barrackkk <kgmlwns904@gmail.com>
2026-01-04 20:16:21 +0900

    [Gold V] Title: 치킨 배달, Time: 200 ms, Memory: 17200 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,70 +1,102 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
-    static List<int[]> chicken = new ArrayList<>();
-    static List<int[]> home = new ArrayList<>();
-    static int[] chickenPick;
-    static int N, M, ans = Integer.MAX_VALUE;
 
-    public static void comb(int cnt, int chickenIdx) {
-        if (cnt == M) {
-            int sum = 0;
+    static int[][] map;
+    static int N, M;
 
-            for (int[] h : home) {
-                int best = Integer.MAX_VALUE;
-                for (int i : chickenPick) {
-                    int[] c = chicken.get(i);
-                    int d = Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]);
-                    best = Math.min(best, d);
+    static List<int[]> chickenXY = new ArrayList<>();
+    static List<int[]> houseXY = new ArrayList<>();
+
+    static int answer = Integer.MAX_VALUE;
+    static boolean[] selected; 
+
+    static int calcCityChickenDistance() {
+        int sum = 0;
+
+        for (int[] h : houseXY) {
+            int hx = h[0], hy = h[1];
+            int best = Integer.MAX_VALUE;
+
+            for (int i = 0; i < chickenXY.size(); i++) {
+                if (!selected[i]) {
+                    continue;
                 }
-                sum += best;
-
-                if (sum >= ans) {
-                    break;
+                int[] c = chickenXY.get(i);
+                int cx = c[0], cy = c[1];
+                int dist = Math.abs(hx - cx) + Math.abs(hy - cy);
+                if (dist < best) {
+                    best = dist;
                 }
             }
 
-            ans = Math.min(ans, sum);
+            sum += best;
+
+            if (sum >= answer) {
+                return sum;
+            }
+        }
+
+        return sum;
+    }
+
+    static void chooseChicken(int start, int picked) {
+        if (picked == M) {
+            answer = Math.min(answer, calcCityChickenDistance());
             return;
         }
 
-        for (int i = chickenIdx; i < chicken.size(); i++) {
-            chickenPick[cnt] = i;
-            comb(cnt + 1, i + 1);
+        if (start == chickenXY.size()) {
+            return;
+        }
+
+        for (int i = start; i < chickenXY.size(); i++) {
+            selected[i] = true;
+            chooseChicken(i + 1, picked + 1);
+            selected[i] = false;
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    static void cntChicken() {
+        chickenXY.clear();
+        houseXY.clear();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 2) {
+                    chickenXY.add(new int[]{i, j});
+                } else if (map[i][j] == 1) {
+                    houseXY.add(new int[]{i, j});
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new int[N][N];
-        chickenPick = new int[M];
+        map = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                int num = Integer.parseInt(st.nextToken());
-                arr[i][j] = num;
-                if (num == 1) {
-                    home.add(new int[]{i, j});
-                } else if (num == 2) {
-                    chicken.add(new int[]{i, j});
-                }
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        comb(0, 0);
-        sb.append(ans);
-        System.out.println(sb);
+        cntChicken();
+
+        selected = new boolean[chickenXY.size()];
+        chooseChicken(0, 0);
+
+        System.out.println(answer);
     }
 }
